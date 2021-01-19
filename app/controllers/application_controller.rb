00f5950f6809a506894396fab74f608e04f84ace
Chris Bellairs <c.m.bellairs@gmail.com>
2021-01-19 21:18:56 +0000

    Challenge completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def ask
+  end
+
+  def answer
+    @answer = if params[:question] == 'I am going to work'
+                'Great!'
+              elsif params[:question].include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
